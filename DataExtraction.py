13d196aa656b8910d34c92ab8704837037826e71
Namrata Simha <npsimha@ucsc.edu>
2016-12-05 01:29:20 -0800

    Added code to find mean and std deviation
    Removed NaN values from VALUENUMs => reduced size of ITEMID list. Fixed the problem in mean code.

--- DataExtraction.py
+++ DataExtraction.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import numpy.ma as ma
+import math
 import os
 import matplotlib.pyplot as plt
 import matplotlib
@@ -44,14 +44,14 @@ chartevents_df = get_chartevents()
 # vitals_labs = pd.merge(vitals_train,labs_train, left_index=True,right_index=True, how='outer')
 vitals_labs_icu = admission_df[admission_df['HOSPITAL_EXPIRE_FLAG'] == 1]
 vitals_labs_icu = vitals_labs_icu[vitals_labs_icu['DIAGNOSIS'] == 'SEPSIS']
-print(vitals_labs_icu)
+#print(vitals_labs_icu)
 
 hospital_ids = vitals_labs_icu['HADM_ID']
-print(hospital_ids)
+#print(hospital_ids)
 
 chartevents_df = chartevents_df[chartevents_df['HADM_ID'].isin(hospital_ids)]
 
-print(chartevents_df)
+#print(chartevents_df)
 item_list = []
 for i in chartevents_df['ITEMID']:
     item_list.append(i)
@@ -59,36 +59,58 @@ value_list = []
 for i in chartevents_df['VALUENUM']:
     value_list.append(i)
 
-#find unique list of itemids for length
+#print("Value_list",value_list)
+
+##find unique list of itemids for length
 item_list_unique = []
 item_list_unique = set(item_list)
 n = len(item_list_unique)
-print ("Unique values count:", n)
+#print ("Unique values count:", n)
 
-item_values = [[], [[] for i in range(13000)]]
+item_values = [[], [[]for i in range(n)]]
 for i in range(0, len(item_list) - 1):
     i_found = 0
     item_values_length = len(item_values[0])
-    for j in range(0, item_values_length - 1):
+    for j in range(0, item_values_length):
         if item_list[i] == item_values[0][j]:
-            item_values[1][j].append(value_list[i])
-            i_found = 1
-            break
+            if not math.isnan(value_list[i]):
+                item_values[1][j].append(value_list[i])
+                i_found = 1
+                break
     if i_found == 0:
-        item_values[0].append(item_list[i])
-        print("Length: ",item_values_length)
-        item_values[1][item_values_length].append(value_list[i])
-        print("Item values after iteration:", i, " is ", item_values)
+        if not math.isnan(value_list[i]):
+            item_values[0].append(item_list[i])
+            #print("Length: ",item_values_length)
+            item_values[1][item_values_length].append(value_list[i])
+            #print("Item values after iteration:", i, " is ", item_values)
 
-item_list_unique_after_picking = []
+"""item_list_unique_after_picking = []
 item_list_unique_after_picking = set(item_values[0])
 n1 = len(item_list_unique_after_picking)
 print ("Unique values count after picking:", n1, "and length of item_values[0] is:",len(item_values[0]))
+"""
 
 mean_list = []
-for i in range(0, len(item_values[0]) - 1):
-    #convert item_values[1][i] into a numpy array
+std_dev_list = []
+for i in range(0, len(item_values[0])):
+    ##convert item_values[1][i] into a numpy array
     valuenum_list = np.array(item_values[1][i])
-    masked_valuenum = ma.masked_equal(valuenum_list, 'nan')
-    mean_list.append(numpy.mean(masked_valuenum))
-print(mean_list)
+    #print("Valuenum_list:",i,":",valuenum_list)
+    #masked_valuenum = ma.masked_equal(valuenum_list, 'nan')
+    mean_list.append(np.mean(valuenum_list))
+    std_dev_list.append(np.std(valuenum_list))
+
+print("Mean list: ",mean_list)
+print("Std Deviation list: ",std_dev_list)
+
+##for logit, SVM and random forests
+final_items_list = []
+
+for i in range(0, len(item_values[0])):
+    temp = []
+    temp.append(item_values[0][i])
+    temp.append(mean_list[i])
+    temp.append(std_dev_list[i])
+    final_items_list.append(temp)
+
+print (final_items_list)
